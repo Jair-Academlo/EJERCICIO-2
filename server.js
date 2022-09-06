3aa248c7e03ac12708338a638b2edf9736411fb5
sasuske <jelver00@hotmail.com>
2022-09-06 13:17:23 -0500

    prueba

---
+++ server.js
@@ -0,0 +1,17 @@
+require('colors');
+const { app } = require('./app');
+const { db } = require('./utils/database');
+
+db.authenticate()
+  .then(() => console.log('datos autenticados'.magenta))
+  .catch(error => console.log(error));
+
+db.sync()
+  .then(() => console.log('datos sincronizados'.magenta))
+  .catch(error => console.log(error));
+
+const PORT = 4300;
+
+app.listen(PORT, () => {
+  console.log(`el servidor se esta ejecutando en el puerto ${PORT}`.cyan);
+});
