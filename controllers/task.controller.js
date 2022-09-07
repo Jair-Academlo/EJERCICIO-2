297f3e876280477863c593d0b659524a4112a8b8
sasuske <jelver00@hotmail.com>
2022-09-07 17:48:37 -0500

    main

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -2,8 +2,13 @@ const { Task } = require('../models/taks.model');
 
 const crearTarea = async (req, res) => {
   try {
-    const { title, userId, limitDate } = req.body;
-    const nuevaTarea = await Task.create({ title, userId, limitDate });
+    const { title, userId, limitDate, startDate } = req.body;
+    const nuevaTarea = await Task.create({
+      title,
+      userId,
+      limitDate,
+      startDate,
+    });
     res.status(201).json({
       message: 'Tarea creada con exito',
       status: 'operacion exitosa',
@@ -14,4 +19,75 @@ const crearTarea = async (req, res) => {
   }
 };
 
-module.exports = { crearTarea };
+const obtenerTareas = async (req, res) => {
+  try {
+    const todasLasTareas = await Task.findAll();
+
+    res.status(200).json({
+      message: 'tareas encontradas',
+      status: 'operacion exitosa',
+      todasLasTareas,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const tareasViaStatus = async (req, res) => {
+  try {
+    const { status } = req.params;
+
+    const viaStatus = await Task.findAll({ where: { status } });
+
+    if (!['Active', 'completed', 'late', 'cancelled'].includes(status)) {
+      return res.status(404).json({
+        message: 'El status no fue encontrado',
+        status: ' operacion fallida',
+      });
+    } else {
+      res.status(200).json({
+        message: 'Estas son las tareas encontradas con el status buscado',
+        status: 'operacion exitosa',
+        viaStatus,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const actualizarTarea = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findOne({ where: { id } });
+
+    const { time } = req.body;
+
+    if (new Date(time) < new Date(task.startDate)) {
+      return res.status(404).json({
+        message: 'la fecha de tarea de inicio es mayor a la de la entrega',
+        status: 'error de tiempos',
+      });
+    } else {
+      await task.update({
+        finishDate: new Date(time),
+        status:
+          new Date(task.limitDate) >= new Date(time) ? 'completed' : 'late',
+      });
+    }
+
+    res.status(200).json({
+      message: 'Bien Hecho, la tarea se ha actualizado',
+      status: 'operacion exitosa',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  crearTarea,
+  obtenerTareas,
+  tareasViaStatus,
+  actualizarTarea,
+};
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { crearTarea } = require('../controllers/task.controller');
+const {
+  crearTarea,
+  obtenerTareas,
+  tareasViaStatus,
+  actualizarTarea,
+} = require('../controllers/task.controller');
 const { createTaskValidator } = require('../middlewares/validator.middleware');
 
 const router = express.Router();
 
 router.post('/', createTaskValidator, crearTarea);
+router.get('/', obtenerTareas);
+router.get('/:status', tareasViaStatus);
+router.patch('/:id', actualizarTarea);
 
 module.exports = { taskRouter: router };
